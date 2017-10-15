2708b0300c19d11e41694ecb2358de04cbc207da
cxb <xb230801@163.com>
2017-10-15 15:45:32 +0800

    redis simple test

--- src/main/java/com/cxb/web/UserController.java
+++ src/main/java/com/cxb/web/UserController.java
@@ -3,8 +3,12 @@ package com.cxb.web;
 import com.cxb.entity.User;
 import com.cxb.mapper.UserMapper;
 import org.apache.ibatis.annotations.Insert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,18 +18,26 @@ import java.util.List;
  */
 @RestController
 public class UserController {
+    private Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserMapper userMapper;
 
     @RequestMapping("/getUsers")
+    @Cacheable(value = "users")
     public List<User> getUsers(){
+        System.out.println("ssss");
+        log.info("aaaaa");
        return userMapper.getAll();
+
     }
 
-    @RequestMapping("/getUser")
+    @RequestMapping(value = "/getUser")
+    @Cacheable(value = "user")
     public User getUser(Long id) {
+        //Long of = Long.valueOf(id);
         User user=userMapper.getUserById(id);
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
         return user;
     }
 
